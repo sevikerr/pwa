2b08d2e5f6591ed5718135ed6d862a1f35608220
Vitalii Sevastianov <sevikv@gmail.com>
2019-04-09 22:31:00 +0300

    add prompt

--- index.js
+++ index.js
@@ -5,3 +5,18 @@ if ('serviceWorker' in navigator) {
       console.log('Service Worker Registered');
     });
 }
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  e.preventDefault();
+  deferredPrompt = e;
+
+  deferredPrompt.prompt();
+  // Wait for the user to respond to the prompt
+  deferredPrompt.userChoice.then((choiceResult) => {
+    if (choiceResult.outcome === 'accepted') {
+      console.log('User accepted the A2HS prompt');
+    } else {
+      console.log('User dismissed the A2HS prompt');
+    }
+  });
+});
